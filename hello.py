a7247ecd8959f860a8fee9049272d201fd8f86bc
Narwhail <john.m.ocampo@gmail.com>
2024-12-08 23:14:47 +0800

    update

--- hello.py
+++ hello.py
@@ -2,4 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title="Cgma Compiler", layout="wide")
 
-st.title("Cgma Compiler Lexical Analyzer")
+st.title("Cgma Compiler")
+st.write("Lexical Analyzer")
