fcfd7c724b6972575e3b6a84722052a71762903f
Narwhail <john.m.ocampo@gmail.com>
2024-12-08 23:23:10 +0800

    adjusted text

--- hello.py
+++ hello.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
 st.set_page_config(page_title="Cgma Compiler", layout="wide")
+st.title("Cgma Compiler")
+st.write("Lexical Analyzer")
 
 col1, col2 = st.columns(2)
 with col1:
-    st.title("Cgma Compiler")
-    st.write("Lexical Analyzer")
-
-    st.button("Run Lexical Analyzer")
+    with st.container(border=True):
+        st.write("Test Lexeme and token")
+        st.button("Run Lexical Analyzer")
 
 with col2:
     with st.container(border=True):
