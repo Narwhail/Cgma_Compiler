604374dbee87ba6fb7f8775c2a6b8d1f3beb5e0d
Narwhail <john.m.ocampo@gmail.com>
2024-12-08 23:13:21 +0800

    imported streamlit

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("finally")
+import streamlit as st
+from streamlit_ace import st_ace
+import subprocess
+
+st.set_page_config(page_title="Cgma Compiler", layout="wide")
+
+st.title("Cgma Compiler Lexical Analyzer")
---
+++ pythu/test.py
@@ -0,0 +1,4 @@
+test = ["""chungus x = 10
+        chungus y = 11"""]
+
+print(test)
