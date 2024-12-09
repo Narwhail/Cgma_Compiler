3f1571cd9c828656bcabe4397ee67a2d4f1dcad7
Narwhail <john.m.ocampo@gmail.com>
2024-12-09 13:01:12 +0800

    Added subheader texts

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@ import lexical.lexer as lexer
 
 st.set_page_config(page_title="Cgma Compiler", layout="wide")
 st.title("Cgma Compiler")
+st.write("A lexical analyzer and syntax analyzer for a new programming language based on C. In compliance for the Automata Theory & Formal Languages.")
 
 column_height = 400
 col1, col2 = st.columns(2)
@@ -13,7 +14,7 @@ with col1:
 
 with col2:
     with st.container(height=column_height, border=True):
-        st.write("Lexical Analyzer")
+        st.subheader("Lexical Analyzer")
         # array tuple format: lexeme(string), token(string), line(int), column(line)
         # textExample = [("chungus", "data_type", 0, 0),("x", "identifier", 0, 8),("=", "assign_op", 0, 9),("10", "int_literal", 0, 10),("hello", "string", 1, 0),("chungus", "data_type", 0, 0),("x", "identifier", 0, 8),("=", "assign_op", 0, 9),("10", "int_literal", 0, 10),("hello", "string", 1, 0)]
 
@@ -23,4 +24,5 @@ with col2:
             st.write(f"Line {line}, Column {column}. Lexeme **{lexeme}** has a token of **{token}**.")
 
 with st.container(border=True):
+    st.subheader("Syntax Analyzer")
     st.write("Code has no error!")
