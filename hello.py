1401689d1c8b9d4ac7b2191fabc14e09092398c5
Narwhail <john.m.ocampo@gmail.com>
2024-12-09 13:04:23 +0800

    aa

--- hello.py
+++ hello.py
@@ -10,7 +10,8 @@ col1, col2 = st.columns(2)
 with col1:
     with st.container(height=column_height, border=True):
         textAreaInput = st.text_area(height=column_height-120, label="Code", placeholder="Enter code...")
-        st.button("Run Lexical Analyzer")
+        if st.button("Run Lexical Analyzer"):
+            buttonInput = textAreaInput
 
 with col2:
     with st.container(height=column_height, border=True):
@@ -18,7 +19,7 @@ with col2:
         # array tuple format: lexeme(string), token(string), line(int), column(line)
         # textExample = [("chungus", "data_type", 0, 0),("x", "identifier", 0, 8),("=", "assign_op", 0, 9),("10", "int_literal", 0, 10),("hello", "string", 1, 0),("chungus", "data_type", 0, 0),("x", "identifier", 0, 8),("=", "assign_op", 0, 9),("10", "int_literal", 0, 10),("hello", "string", 1, 0)]
 
-        lexicalResult = lexer.analyzeLexical(textAreaInput)
+        lexicalResult = lexer.analyzeLexical(buttonInput)
 
         for lexeme, token, line, column in lexicalResult:
             st.write(f"Line {line}, Column {column}. Lexeme **{lexeme}** has a token of **{token}**.")
