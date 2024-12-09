e9d0e36533eadcf88004303aba2d4fc8ebe208ce
Narwhail <john.m.ocampo@gmail.com>
2024-12-09 12:02:30 +0800

    aa

--- hello.py
+++ hello.py
@@ -17,7 +17,8 @@ with col2:
         st.write("Lexical Analyzer")
         # array tuple format: lexeme(string), token(string), line(int), column(line)
         # textExample = [("chungus", "data_type", 0, 0),("x", "identifier", 0, 8),("=", "assign_op", 0, 9),("10", "int_literal", 0, 10),("hello", "string", 1, 0),("chungus", "data_type", 0, 0),("x", "identifier", 0, 8),("=", "assign_op", 0, 9),("10", "int_literal", 0, 10),("hello", "string", 1, 0)]
-        textExample = lexer.analyzeLexical(textAreaInput)
+        sample = "chungus skibidi"
+        textExample = lexer.analyzeLexical(sample)
         for lexeme, token, line, column in textExample:
             st.write(f"Line {line}, Column {column}. Lexeme **{lexeme}** has a token of **{token}**.")
 
