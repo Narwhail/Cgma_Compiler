ee1269fb5e4f5c7740792963a5e37d968eaba2cb
Narwhail <john.m.ocampo@gmail.com>
2024-12-09 12:23:12 +0800

    moved lexer to main file

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
 import streamlit as st # type: ignore
-import lexical.lexer as lexer
 
 st.set_page_config(page_title="Cgma Compiler", layout="wide")
 st.title("Cgma Compiler")
@@ -17,11 +16,35 @@ with col2:
         # array tuple format: lexeme(string), token(string), line(int), column(line)
         # textExample = [("chungus", "data_type", 0, 0),("x", "identifier", 0, 8),("=", "assign_op", 0, 9),("10", "int_literal", 0, 10),("hello", "string", 1, 0),("chungus", "data_type", 0, 0),("x", "identifier", 0, 8),("=", "assign_op", 0, 9),("10", "int_literal", 0, 10),("hello", "string", 1, 0)]
 
-        xoxo = textAreaInput.splitlines()
-        yoyo = lexer.analyzeLexical(xoxo)
+        lexicalText = analyzeLexical(textAreaInput)
 
-        # for lexeme, token, line, column in yoyo:
-        #     st.write(f"Line {line}, Column {column}. Lexeme **{lexeme}** has a token of **{token}**.")
+        for lexeme, token, line, column in lexicalText:
+            st.write(f"Line {line}, Column {column}. Lexeme **{lexeme}** has a token of **{token}**.")
 
 with st.container(border=True):
     st.write("Code has no error!")
+
+
+def analyzeLexical(textInput):
+    lines = textInput.splitlines()  # Split input into lines
+    tokens = []
+    
+    for line_num, line in enumerate(lines, 1):  # line_num starts at 1
+        temp_str = ""
+        column_num = 1  # Start column at 1 for each line
+        
+        for char in line:
+            if char == " ":
+                if temp_str:  # Only add token if temp_str is not empty
+                    tokens.append((temp_str, "Identifier", line_num, column_num - len(temp_str)))
+                    temp_str = ""  # Reset for the next token
+                # Skip the space, no token to append
+            else:
+                temp_str += char  # Build the current token
+            column_num += 1  # Move to the next character (column)
+        
+        # After processing the line, check if there is any leftover token
+        if temp_str:
+            tokens.append((temp_str, "Identifier", line_num, column_num - len(temp_str)))
+    
+    return tokens
